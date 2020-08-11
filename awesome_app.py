a8c7912c5f8f6b7218cb5864459eb88a4e8f1cc8
eugenechia95 <eugenechia95@gmail.com>
2020-08-11 14:31:43 +0800

    Refactored code

--- awesome_app.py
+++ awesome_app.py
@@ -1,40 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-# st.title('Awesome AI App')
-
-# DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-# @st.cache
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase, axis='columns', inplace=True)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     return data
-
-# data_load_state = st.text('Loading data...')
-# data = load_data(10000)
-# data_load_state.text("Done! (using st.cache)")
-
-# if st.checkbox('Show raw data'):
-#     st.subheader('Raw data')
-#     st.write(data)
-
-# st.subheader('Number of pickups by hour')
-# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-# st.bar_chart(hist_values)
-
-# # Some number in the range 0-23
-# hour_to_filter = st.slider('hour', 0, 23, 17)
-# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-# st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-# st.map(filtered_data)
-
+import joblib
 
 html_temp = """
 <div style="background-color:tomato;padding:10px">
@@ -42,15 +9,29 @@ html_temp = """
 </div>
 """
 st.markdown(html_temp,unsafe_allow_html=True)
-# load_css('icon.css')
-# load_icon('people')
+
+def load_css(file_name):
+  with open(file_name) as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
-uploaded_file = st.file_uploader("Upload your Image")
-if uploaded_file is not None:
-  # data = pd.read_csv(uploaded_file)
-  st.image(uploaded_file, caption='Sameple image', use_column_width=True)
-if st.button("Predict"):
-  st.text("6 cells")
+# Can try loading trained pickle models in this format
+# gender_nv_model = open("models/naivebayesgendermodel.pkl","rb")
+# gender_clf = joblib.load(gender_nv_model)
 
+def main():
+  load_css('style.css')
+
+  st.set_option('deprecation.showfileUploaderEncoding', False)
+  uploaded_file = st.file_uploader("Upload your Image")
+  if uploaded_file is not None:
+    # data = pd.read_csv(uploaded_file)
+    st.image(uploaded_file, caption='Sameple image', use_column_width=True)
+
+  if st.button("Predict"):
+    '''
+    Can run model predict method here
+    '''
+    st.text("6 cells")
+
+main()
